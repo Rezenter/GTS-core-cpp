76d7c1c2c3123dc3cbd3b344220b52899683d334
rezenter <nisovru@gmail.com>
2022-03-25 23:00:59 +0300

    server initial

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,15 @@
 #include "version.h"
 #include "include/FastADC/FastSystem.h"
 
+#include <iostream>
+#include <string>
+#include "Server/server.hpp"
+
+#include <filesystem>
+
 int main([[maybe_unused]] int argc,[[maybe_unused]] char* argv[]) {
     std::cout << "TS fast acquisition, revision:" << REVISION << std::endl << std::flush;
+    std::cout << std::filesystem::current_path() << '\n' <<std::flush;
 
     //FastADC legacy
     Config config;
@@ -17,6 +24,18 @@ int main([[maybe_unused]] int argc,[[maybe_unused]] char* argv[]) {
     while (!fs.exitRequested()){
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
+
+
+    //Server
+    try{
+        http::server::server s("192.168.10.41", "8080", "html/");
+        s.run();
+    }
+    catch (std::exception& e){
+        std::cerr << "exception: " << e.what() << "\n";
+    }
+
+
 
     std::cout << "\nNormal exit." << std::endl << std::flush;
 }
